d29ae723c140de40971633025c72ddc4df0a50ac
aikaterna <20862007+aikaterna@users.noreply.github.com>
2024-12-22 14:15:43 -0800

    Bump YT plugin version (#6490)

--- redbot/cogs/audio/managed_node/version_pins.py
+++ redbot/cogs/audio/managed_node/version_pins.py
@@ -6,4 +6,4 @@ __all__ = ("JAR_VERSION", "YT_PLUGIN_VERSION")
 
 
 JAR_VERSION: Final[LavalinkVersion] = LavalinkVersion(3, 7, 12, red=1)
-YT_PLUGIN_VERSION: Final[str] = "1.11.1"
+YT_PLUGIN_VERSION: Final[str] = "1.11.2"
